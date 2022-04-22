af882cf91cda04f6293651c6327cf02353fcceb9
sharosoo <shiningmoon@snu.ac.kr>
2022-04-22 12:00:51 +0900

    add comment on signup serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,6 +5,14 @@ from rest_framework import serializers
 from profiles.models import Profile
 
 
+def get_user_id_or_raise_type_error(**user_data):
+    try:
+        user_instance = get_user_model().objects.create(**user_data)
+        return user_instance.id
+    except TypeError:
+        raise TypeError
+
+
 class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
@@ -30,16 +38,16 @@ class SignupSerializer(serializers.ModelSerializer):
         field = ('user', 'nickname', 'desc', 'image')
 
     def create(self, validated_data):
-
+        # nested serializer에서 user serializer data를 뽑아낸다
         user_data = validated_data.pop('user')
 
-        try:
-            user_instance = get_user_model().objects.create(**user_data)
-        except TypeError:
-            raise TypeError
+        # user serializer data로 user instance 생성을 시도한다.
+        user_id = get_user_id_or_raise_type_error(**user_data)
 
-        validated_data['user'] = f'{user_instance.id}'
+        # user id를 profile serializer data에 삽입한다.
+        validated_data['user'] = f'{user_id}'
 
+        # profile을 생성한다.
         profile_instance = Profile.objects.create(**validated_data)
 
         return profile_instance
